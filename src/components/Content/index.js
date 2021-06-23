cbaa5af5f8e1278a91601e5e37d057e028680614
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-06-22 22:15:48 -0300

    commit inicial

---
+++ src/components/Content/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Content = ({ children }) => {
+  return <div className="content">{children}</div>;
+};
+
+export default Content;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+      </ul>
+
+      <div class="legal">
+        &copy; 2021 Cão sem casa. Todos os direitos reservados.
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
